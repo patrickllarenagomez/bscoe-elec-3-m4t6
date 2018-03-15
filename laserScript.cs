380592e2a235f432f2f754793b2e388a247d0cbf
unknown <patrickllarenagomez@gmail.com>
2018-03-15 09:15:41 +0800

    * Explosion and Scoring

---
+++ laserScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class laserScript : MonoBehaviour {
+
+    // Use this for initialization
+    
+    public GameObject lastShipHit;
+    public GameObject explosionEnemy;
+    Text scoreText;
+    GameObject canvas;
+    int score = 0;
+    int counterShip;
+    AudioSource explodeSFX;
+    GameObject explosion;
+    void Start () {
+        canvas = GameObject.Find("Canvas");
+        scoreText = canvas.GetComponentInChildren<Text>();
+        explosion = GameObject.Find("explosionsfx");
+        explodeSFX = explosion.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    void AddScore()
+    {
+        score++;
+        scoreText.text = "Score :" + score;
+    }   
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            if(lastShipHit == other)
+            {
+                // increment if the same ship was hit
+                counterShip++;
+            }
+            else
+            {
+                //0 if the last ship hit wasn't the previous one
+                counterShip = 0;
+            }
+
+            if(counterShip > 3)
+            {
+                Instantiate(Resources.Load("explosionEnemy"), other.transform.position, other.transform.rotation);
+                AddScore();
+                if (!explodeSFX.isPlaying)
+                {
+                    explodeSFX.Play();
+                }
+                Destroy(other);
+                Destroy(other.transform.parent.gameObject);
+            }
+        }
+        lastShipHit = other;
+    }
+}
